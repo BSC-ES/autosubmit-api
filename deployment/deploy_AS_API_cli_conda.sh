24f4b2e64d88464137b4068235de55b48baaf00b
Luiggi Tenorio <luiggi.tenorio@bsc.es>
2023-11-20 09:52:00 +0100

    update dev deployment script

--- deployment/deploy_AS_API_cli_conda.sh
+++ deployment/deploy_AS_API_cli_conda.sh
@@ -13,15 +13,16 @@ while getopts ":e:u" opt; do
    esac
 done
 
+# activate conda environment
+conda activate autosubmit_api
 
 # Stop current instance of unicorn
 # pstree -ap | grep gunicorn | awk -F',' '{print $2}' | awk -F' ' '{print $1}' | head -n 1 | xargs -r kill
+echo "Stopping current instances of autosubmit_api..."
 pstree -ap | grep autosubmit_api | awk -F',' '{print $2}' | awk -F' ' '{print $1}' | head -n 1 | xargs -r kill
+sleep 3
 pstree -ap | grep autosubmit_api 
 # pstree -ap | grep gunicorn 
-
-# activate conda environment
-conda activate autosubmit_api
 
 # if update to a new version we install it from pip
 if [ "${UPDATE}" = true ]; then
@@ -30,22 +31,16 @@ fi
 
 # prepare to launch
 echo "Set SECRET KEY"
-export AUTHORIZATION_LEVEL='NONE'
+export PROTECTION_LEVEL='ALL'
 echo "Set SECRET KEY"
 export SECRET_KEY='c&X=<dFoz^ym}_^{jvma'
 echo "Set CAS LOGIN URL"
 export CAS_LOGIN_URL='https://cas.bsc.es/cas/login'
 echo "Set CAS VERIFY URL"
 export CAS_VERIFY_URL='https://cas.bsc.es/cas/serviceValidate'
-echo "Gunicorn starting in 3 seconds..."
-sleep 3
+echo "Gunicorn starting in 2 seconds..."
+sleep 2
 LOG_NAME="$(date '+%Y%m%d%H%M')logunicorn.yml"
 echo "${LOG_PATH}/${LOG_NAME}"
 
-# NOTE: for debugging remove the --preload option from the command line invokation
-
-autosubmit_api start --log-file ${LOG_PATH}/${LOG_NAME} -w 6 -b 127.0.0.1:8081 --daemon
-
-# gunicorn --log-file=-  --error-logfile ${LOG_PATH}/${LOG_NAME} --timeout 600 -b 0.0.0.0:8081 -w 6 "AS_API_4.autosubmit_api.app:create_app()"  --preload --daemon
-
-#set +xv
+autosubmit_api start --log-file ${LOG_PATH}/${LOG_NAME} -w 2 -b 0.0.0.0:8081 --daemon --log-level DEBUG
