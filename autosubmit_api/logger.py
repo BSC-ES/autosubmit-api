a82168b882292970594495ab0496ff448f37980a
Luiggi Tenorio <luiggi.tenorio@bsc.es>
2023-12-11 17:22:11 +0100

    add method to logger

--- autosubmit_api/logger.py
+++ autosubmit_api/logger.py
@@ -6,37 +6,45 @@ import traceback
 from flask import request
 
 
-def with_log_run_times(_logger: logging.Logger, _tag: str = "", catch_exc:bool=False):
+def with_log_run_times(
+    _logger: logging.Logger, _tag: str = "", catch_exc: bool = False
+):
     """
     Function decorator to log runtimes of the endpoints
     :param _logger: logger to use
     :param _tag: tag for the logs
     :param catch_exc: if True, will catch any Exception and not raise it
     """
+
     def decorator(func):
         @wraps(func)
         def inner_wrapper(*args, **kwargs):
             start_time = time.time()
-            path = ""
             try:
                 path = request.full_path
+                method = request.method
             except:
-                pass
-            _logger.info('{}|RECEIVED|{}'.format(_tag, path))
+                path = ""
+                method = ""
+            _logger.info("{}|RECEIVED|{}{}".format(_tag, method, path))
             response = None
             try:
                 response = func(*args, **kwargs)
             except Exception as exc:
-                _logger.error('{}|ERROR|{}|Exception msg: {}'.format(_tag, path, exc))
+                _logger.error("{}|ERROR|{}|Exception msg: {}".format(_tag, path, exc))
                 if catch_exc:
                     _logger.error(traceback.format_exc())
                 else:
                     raise exc
-            _logger.info('{}|RTIME|{}|{:.3f}'.format(
-                _tag, path, (time.time() - start_time)))
+            _logger.info(
+                "{}|RTIME|{}{}|{:.3f}".format(
+                    _tag, method, path, (time.time() - start_time)
+                )
+            )
             return response
 
         return inner_wrapper
+
     return decorator
 
 
@@ -44,7 +52,7 @@ def get_app_logger() -> logging.Logger:
     """
     Returns app logger
     """
-    _logger = logging.getLogger('gunicorn.error')
+    _logger = logging.getLogger("gunicorn.error")
     return _logger
 
 
