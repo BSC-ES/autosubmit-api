085dbfc8040a547dfd2e76a5ef7217c88f2a8b83
Luiggi Tenorio <luiggi.tenorio@bsc.es>
2024-05-10 15:24:50 +0200

    reduce status_updater writes

--- tests/bgtasks/test_status_updater.py
+++ tests/bgtasks/test_status_updater.py
@@ -6,22 +6,25 @@ from autosubmit_api.database.common import (
 )
 from autosubmit_api.history.database_managers.database_models import RunningStatus
 
-class TestStatusUpdater:
 
+class TestStatusUpdater:
     def test_same_tables(self, fixture_mock_basic_config):
         prepare_db()
-        
+
+        with create_as_times_db_engine().connect() as conn:
+            exps_status = conn.execute(tables.experiment_status_table.delete())
+
         StatusUpdater.run()
-        
+
         with create_autosubmit_db_engine().connect() as conn:
             experiments = conn.execute(tables.experiment_table.select()).all()
-        
+
         with create_as_times_db_engine().connect() as conn:
             exps_status = conn.execute(tables.experiment_status_table.select()).all()
 
         assert len(experiments) == len(exps_status)
         assert set([x.id for x in experiments]) == set([x.exp_id for x in exps_status])
         assert set([x.name for x in experiments]) == set([x.name for x in exps_status])
-        
+
         for e_st in exps_status:
             assert e_st.status in [RunningStatus.RUNNING, RunningStatus.NOT_RUNNING]
