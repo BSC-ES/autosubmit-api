f465b31163826dcc6230c0c3f4f8e117c76dcc74
Luiggi Tenorio <luiggibit@gmail.com>
2024-12-13 14:07:15 +0100

    Ensure networkx pkl file support (#158)
    * add 4.1.11 test with networkx pkl file
    
    * include autosubmit module

--- tests/test_persistance.py
+++ tests/test_persistance.py
@@ -1,17 +1,13 @@
 from autosubmit_api.persistance.pkl_reader import PklReader
+import pytest
 
 
 class TestPklReader:
-
-    def test_reader(self, fixture_mock_basic_config):
-        test_cases = [
-            {"expid": "a003", "size": 8},
-            {"expid": "a007", "size": 8},
-            {"expid": "a3tb", "size": 55},
-        ]
-
-        for exp in test_cases:
-            content = PklReader(exp["expid"]).parse_job_list()
-            assert len(content) == exp["size"]
-            for item in content:
-                assert item.name.startswith(exp["expid"])
+    @pytest.mark.parametrize(
+        "expid, size", [("a003", 8), ("a007", 8), ("a3tb", 55), ("a1ve", 8)]
+    )
+    def test_reader(self, fixture_mock_basic_config, expid, size):
+        content = PklReader(expid).parse_job_list()
+        assert len(content) == size
+        for item in content:
+            assert item.name.startswith(expid)
