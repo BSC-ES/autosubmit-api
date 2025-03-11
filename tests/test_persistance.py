36c0327be078d661c6083b7835baa60b9a58cf7d
ltenorio <luiggi.tenorio@bsc.es>
2025-03-11 16:10:10 +0100

    add test new pkl format

--- tests/test_persistance.py
+++ tests/test_persistance.py
@@ -4,7 +4,7 @@ import pytest
 
 class TestPklReader:
     @pytest.mark.parametrize(
-        "expid, size", [("a003", 8), ("a007", 8), ("a3tb", 55), ("a1ve", 8)]
+        "expid, size", [("a003", 8), ("a007", 8), ("a3tb", 55), ("a1ve", 8), ("a1vj", 8)]
     )
     def test_reader(self, fixture_mock_basic_config, expid, size):
         content = PklReader(expid).parse_job_list()
