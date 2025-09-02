ad0149f12bb1aeba38bf755a4b60a6bffb2033d9
ltenorio <luiggi.tenorio@bsc.es>
2025-09-02 09:46:51 +0200

    update experiment structure handler

--- autosubmit_api/repositories/experiment_structure.py
+++ autosubmit_api/repositories/experiment_structure.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
 from typing import List
 
 from pydantic import BaseModel
-from sqlalchemy import Engine, Table
+from sqlalchemy import Engine, Table, select
 
 from autosubmit_api.database import tables
 from autosubmit_api.database.common import (
@@ -34,7 +35,7 @@ class ExperimentStructureSQLRepository(ExperimentStructureRepository):
 
     def get_all(self):
         with self.engine.connect() as conn:
-            statement = self.table.select()
+            statement = select(self.table.c.e_from, self.table.c.e_to)
             result = conn.execute(statement).all()
         return [
             ExperimentStructureModel(e_from=row.e_from, e_to=row.e_to) for row in result
@@ -42,8 +43,12 @@ class ExperimentStructureSQLRepository(ExperimentStructureRepository):
 
 
 def create_experiment_structure_repository(expid: str) -> ExperimentStructureRepository:
-    engine = create_sqlite_db_engine(
-        ExperimentPaths(expid).structure_db, read_only=True
-    )
-    table = tables.ExperimentStructureTable
+    exp_paths = ExperimentPaths(expid)
+
+    if Path(exp_paths.db_dir).exists():
+        engine = create_sqlite_db_engine(exp_paths.job_list_db, read_only=True)
+        table = tables.ExperimentStructureDBTable
+    else:
+        engine = create_sqlite_db_engine(exp_paths.structure_db, read_only=True)
+        table = tables.ExperimentStructureTable
     return ExperimentStructureSQLRepository(expid, engine, table)
