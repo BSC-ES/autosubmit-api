d9d25ee232456cf1ff55c7f4504ca3c4816510b3
ltenorio <luiggi.tenorio@bsc.es>
2025-09-02 13:40:31 +0200

    update handler of wrappers

--- autosubmit_api/repositories/job_packages.py
+++ autosubmit_api/repositories/job_packages.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import List, Any
+from pathlib import Path
+from typing import Any, List
+
 from pydantic import BaseModel
 from sqlalchemy import Engine, Table
+
 from autosubmit_api.database import tables
 from autosubmit_api.database.common import (
     create_sqlite_db_engine,
@@ -24,7 +27,8 @@ class JobPackagesRepository(ABC):
 
 
 class JobPackagesSQLRepository(JobPackagesRepository):
-    def __init__(self, engine: Engine, table: Table):
+    def __init__(self, expid: str, engine: Engine, table: Table):
+        self.expid = expid
         self.engine = engine
         self.table = table
 
@@ -34,7 +38,7 @@ class JobPackagesSQLRepository(JobPackagesRepository):
             result = conn.execute(statement).all()
         return [
             JobPackageModel(
-                exp_id=row.exp_id,
+                exp_id=row.exp_id if hasattr(row, "exp_id") else self.expid,
                 package_name=row.package_name,
                 job_name=row.job_name,
             )
@@ -42,14 +46,20 @@ class JobPackagesSQLRepository(JobPackagesRepository):
         ]
 
 
-def create_job_packages_repository(expid: str, wrapper=False) -> JobPackagesRepository:
+def create_job_packages_repository(expid: str, preview=False) -> JobPackagesRepository:
     """
     Create a job packages repository.
 
-    :param wrapper: Whether to use the alternative wrapper job packages table.
+    :param preview: Whether to use the alternative preview table.
     """
-    engine = create_sqlite_db_engine(
-        ExperimentPaths(expid).job_packages_db, read_only=True
-    )
-    table = tables.wrapper_job_package_table if wrapper else tables.job_package_table
-    return JobPackagesSQLRepository(engine, table)
+    exp_paths = ExperimentPaths(expid)
+
+    if Path(exp_paths.db_dir).exists():
+        engine = create_sqlite_db_engine(exp_paths.job_list_db, read_only=True)
+        table = tables.PreviewWrapperJobsTable if preview else tables.WrapperJobsTable
+    else:
+        engine = create_sqlite_db_engine(exp_paths.job_packages_db, read_only=True)
+        table = (
+            tables.wrapper_job_package_table if preview else tables.job_package_table
+        )
+    return JobPackagesSQLRepository(expid, engine, table)
